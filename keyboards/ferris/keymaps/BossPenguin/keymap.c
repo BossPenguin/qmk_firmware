b3020bde8c51230a9f741f09152912d49a940db1
BossPenguin <academics@bosspenguin.de>
2022-06-12 13:20:33 +0200

    Update keymap.c

--- keyboards/ferris/keymaps/BossPenguin/keymap.c
+++ keyboards/ferris/keymaps/BossPenguin/keymap.c
@@ -17,6 +17,9 @@ enum custom_keycodes {
 
 enum combos {
     COMBO_LAYER3,
+    COMBO_LAYER4,
+    COMBO_DEFAULT,
+    COMBO_COLEMAK,
     /*COMBO_UP,
     COMBO_LEFT,
     COMBO_DOWN,
@@ -35,6 +38,9 @@ uint16_t COMBO_LEN = COMBO_LENGTH; // Nifty Trick continued
 
 // Combo key definition
 const uint16_t PROGMEM backspacespace_combo[] = {KC_BSPC, KC_SPC, COMBO_END};
+const uint16_t PROGMEM qwop_combo[] = {KC_Q, KC_W, KC_O, KC_P, COMBO_END}
+const uint16_t PROGMEM fbspcspcj_combo[] = {KC_F, KC_BSPC, KC_SPC, KC_J, COMBO_END};
+const uint16_t PROGMEM zbspcspcslash_combo[] = {KC_Z, KC_BSPC, KC_SPC, KC_SLSH, COMBO_END};
 /*const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM kl_combo[] = {KC_K, KC_L, COMBO_END};
@@ -48,6 +54,9 @@ const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};*/
 // Combo key mapping...
 combo_t key_combos[] = {
     [COMBO_LAYER3] = COMBO(backspacespace_combo, TT(3)),
+    [COMBO_LAYER4] = COMBO(tbspcspcy_combo, TG(4)),
+    [COMBO_LAYER0] = COMBO(zbspcspcslash_combo, DF(0)),
+    [COMBO_COLEMAK] = COMBO(fbspcspcj_combo, DF(5)),
     /*[COMBO_UP] = COMBO(io_combo, KC_UP),
     [COMBO_LEFT] = COMBO(jk_combo, KC_LEFT),
     [COMBO_DOWN] = COMBO(kl_combo, KC_DOWN),
@@ -68,6 +77,8 @@ combo_t key_combos[] = {
 
 [_LAYER3] = LAYOUT(KC_ESC, KC_BTN2, KC_MS_UP, KC_BTN1, KC_WH_U, KC_HOME, KC_END, KC_UP, KC_DEL, KC_BSPC, KC_TAB, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_MPLY, KC_LEFT, KC_DOWN, KC_RIGHT, KC_ENT, KC_ACL0, LGUI(KC_L), KC_PSCR, KC_WH_L, KC_WH_R, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_ACL2, KC_NO, KC_TRNS, KC_TRNS, KC_NO),
 
-[_LAYER4] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_H, KC_J, KC_K, KC_L, LSFT_T(KC_SCLN), KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_N, LGUI_T(KC_M), LALT_T(KC_COMM), RCTL_T(KC_DOT), RALT_T(KC_SLSH), KC_T, KC_SPC, KC_ESC, KC_I)
+[_LAYER4] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_H, KC_J, KC_K, KC_L, LSFT_T(KC_SCLN), KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_N, LGUI_T(KC_M), LALT_T(KC_COMM), RCTL_T(KC_DOT), RALT_T(KC_SLSH), KC_T, KC_SPC, KC_ESC, KC_I),
+
+ [_LAYER5] = LAYOUT(KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SEMI, LSFT_T(KC_A), KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, LSFT_T(KC_O), LCTL_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_K, LGUI_T(KC_M), LALT_T(KC_COMM), RCTL_T(KC_DOT), RALT_T(KC_SLSH), TT(2), KC_BSPC, KC_SPC, MO(1))
 
 };
